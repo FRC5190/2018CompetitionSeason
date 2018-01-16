c8d7e7a4efd72c607077a6c384616eefab31c443
Prateek M <prateek.machiraju@outlook.com>
2018-01-15 19:06:57 -0500

    Implemented turn to angle

---
+++ src/main/kotlin/frc/team5190/robot/navigation/TRNController.kt
@@ -0,0 +1,51 @@
+package frc.team5190.robot.navigation
+
+import com.ctre.phoenix.motorcontrol.ControlMode
+import edu.wpi.first.wpilibj.PIDController
+import edu.wpi.first.wpilibj.PIDOutput
+import edu.wpi.first.wpilibj.livewindow.LiveWindow
+import frc.team5190.robot.drive.DriveTrain
+
+class TRNController(targetAngleDegrees: Double) : PIDOutput {
+
+    private var rotateToAngleRate = 0.0
+
+    private val p = 0.0
+    private val i = 0.0
+    private val d = 0.0
+    private val f = 0.0
+
+    private val target = targetAngleDegrees
+
+    private val pidController = PIDController(p, i, d, f, DriveTrain.navX, this)
+
+    init {
+        pidController.setInputRange(-180.0, 180.0)
+        pidController.setOutputRange(-1.0, 1.0)
+        pidController.setAbsoluteTolerance(2.0)
+        pidController.setContinuous(true)
+        pidController.disable()
+
+        LiveWindow.add(pidController)
+    }
+
+    fun enable() {
+        pidController.setpoint = target
+        rotateToAngleRate = 0.0
+        pidController.enable()
+    }
+
+    fun rotateToAngle() {
+        val left = rotateToAngleRate
+        val right = rotateToAngleRate
+
+        DriveTrain.tankDrive(left, right, ControlMode.PercentOutput)
+    }
+
+    fun hasFinished() = Math.abs(target - DriveTrain.navX.angle) < 1
+
+    override fun pidWrite(output: Double) {
+        rotateToAngleRate = output
+    }
+
+}
