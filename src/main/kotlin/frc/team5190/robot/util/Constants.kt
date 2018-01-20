ce470e3eb36115f9e6675151f4771157bb7e54c8
Prateek M <prateek.machiraju@outlook.com>
2018-01-19 19:29:42 -0500

    #2 Changes for new robot

--- src/main/kotlin/frc/team5190/robot/util/Constants.kt
+++ src/main/kotlin/frc/team5190/robot/util/Constants.kt
@@ -11,20 +11,20 @@ import com.ctre.phoenix.motorcontrol.ControlMode
  * Contains Motor IDs.
  */
 object MotorIDs {
-    val FRONT_LEFT = 1
-    val FRONT_RIGHT = 3
-    val REAR_LEFT = 2
-    val REAR_RIGHT = 4
+    const val FRONT_LEFT = 6
+    const val FRONT_RIGHT = 5
+    const val REAR_LEFT = 3
+    const val REAR_RIGHT = 4
 }
 
 /**
  * Contains constants related to hardware.
  */
 object Hardware {
-    val NATIVE_UNITS_PER_ROTATION = 1440
-    val WHEEL_RADIUS = 3.75 / 2.0
+    const val NATIVE_UNITS_PER_ROTATION = 1440
+    const val WHEEL_RADIUS = 3.0
 
-    val MAX_RPM = 1065
+    const val MAX_RPM = 1065
     val MAX_NATIVE_UNITS_PER_100_MS = Maths.rpmToNativeUnitsPer100Ms(MAX_RPM.toDouble(), WHEEL_RADIUS)
 }
 
