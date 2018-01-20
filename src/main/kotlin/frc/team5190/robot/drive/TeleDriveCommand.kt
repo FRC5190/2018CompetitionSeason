50ec37cf7cd235d790633884d30ace76553c6256
Prateek M <prateek.machiraju@outlook.com>
2018-01-20 10:22:17 -0500

    #2 Fixed command

--- src/main/kotlin/frc/team5190/robot/drive/TeleDriveCommand.kt
+++ src/main/kotlin/frc/team5190/robot/drive/TeleDriveCommand.kt
@@ -8,6 +8,7 @@ package frc.team5190.robot.drive
 import com.ctre.phoenix.motorcontrol.ControlMode
 import edu.wpi.first.wpilibj.command.Command
 import frc.team5190.robot.MainXbox
+import frc.team5190.robot.getLeftX
 import frc.team5190.robot.getLeftY
 import frc.team5190.robot.getRightY
 
@@ -21,13 +22,12 @@ class TeleDriveCommand : Command() {
      * Called periodically until the command is finished or until interrupted.
      */
     override fun execute() {
-        DriveSubsystem.falconDrive.tankDrive(ControlMode.PercentOutput, -MainXbox.getLeftY(), MainXbox.getRightY(), false)
-//        val mode = ControlMode.PercentOutput
-//        when (DriveSubsystem.controlMode) {
-//            DriveMode.ARCADE -> DriveSubsystem.falconDrive.arcadeDrive(-MainXbox.getLeftY(), MainXbox.getLeftX())
-//            DriveMode.TANK -> DriveSubsystem.falconDrive.tankDrive(mode, -MainXbox.getLeftY(), MainXbox.getRightY(), false)
-//            DriveMode.CURVE -> DriveSubsystem.falconDrive.curvatureDrive(mode, -MainXbox.getLeftY(), MainXbox.getLeftX(), MainXbox.aButton)
-//        }
+        val mode = ControlMode.PercentOutput
+        when (DriveSubsystem.controlMode) {
+            DriveMode.ARCADE -> DriveSubsystem.falconDrive.arcadeDrive(-MainXbox.getLeftY(), MainXbox.getLeftX())
+            DriveMode.TANK -> DriveSubsystem.falconDrive.tankDrive(mode, -MainXbox.getLeftY(), -MainXbox.getRightY(), false)
+            DriveMode.CURVE -> DriveSubsystem.falconDrive.curvatureDrive(mode, -MainXbox.getLeftY(), MainXbox.getLeftX(), MainXbox.aButton)
+        }
     }
 
     override fun isFinished() = false
