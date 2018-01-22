618d8930379b434257d6b7bf7b3f8283d2b21002
Prateek M <prateek.machiraju@outlook.com>
2018-01-21 21:51:04 -0500

    Fixed config

--- src/main/kotlin/frc/team5190/robot/drive/FalconDrive.kt
+++ src/main/kotlin/frc/team5190/robot/drive/FalconDrive.kt
@@ -56,7 +56,7 @@ class FalconDrive(val leftMotors: List<WPI_TalonSRX>,
         rightSlaves.forEach { it.follow(rightMaster) }
 
         allMasters.forEach {
-            it.configurePIDF(2.0, 0.0, 0.0, 1.0, rpm = Hardware.MAX_RPM.toDouble(), sensorUnitsPerRotation = Hardware.NATIVE_UNITS_PER_ROTATION.toDouble())
+            it.configurePIDF(0.0, 0.0, 0.0, 0.0, rpm = Hardware.MAX_RPM.toDouble(), sensorUnitsPerRotation = Hardware.NATIVE_UNITS_PER_ROTATION.toDouble())
             it.configMotionProfileTrajectoryPeriod(10, 10)
             it.setStatusFramePeriod(StatusFrameEnhanced.Status_10_MotionMagic, 10, 10)
             it.configNeutralDeadband(0.04, 10)
