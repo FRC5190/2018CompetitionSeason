41622d6d6bc90e6c7d958a019a161e24ff3df9f7
Prateek M <prateek.machiraju@outlook.com>
2018-01-26 10:58:24 -0500

    Added TODO

--- src/main/kotlin/frc/team5190/robot/drive/TeleDriveCommand.kt
+++ src/main/kotlin/frc/team5190/robot/drive/TeleDriveCommand.kt
@@ -19,7 +19,9 @@ class TeleDriveCommand : Command() {
      * Called periodically until the command is finished or until interrupted.
      */
     override fun execute() {
+        // TODO Configure Velocity Drive
         val mode = ControlMode.PercentOutput
+        
         when (DriveSubsystem.controlMode) {
             DriveMode.ARCADE -> DriveSubsystem.falconDrive.arcadeDrive(-MainXbox.getLeftY(), MainXbox.getLeftX())
             DriveMode.TANK -> DriveSubsystem.falconDrive.tankDrive(mode, -MainXbox.getLeftY(), -MainXbox.getRightY(), false)
