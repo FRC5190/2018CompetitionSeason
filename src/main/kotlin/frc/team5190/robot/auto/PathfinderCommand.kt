0e1a7d748b8cc51a0f56edbb93c7fd66011049b5
Ryan Segerstrom <13050203+ThePrimedTNT@users.noreply.github.com>
2018-03-01 11:12:34 -0500

    ez fixes

--- src/main/kotlin/frc/team5190/robot/auto/PathfinderCommand.kt
+++ src/main/kotlin/frc/team5190/robot/auto/PathfinderCommand.kt
@@ -1,8 +1,7 @@
 package frc.team5190.robot.auto
 
 import com.ctre.phoenix.motorcontrol.ControlMode
-import edu.wpi.first.wpilibj.PIDController
-import edu.wpi.first.wpilibj.PIDOutput
+import edu.wpi.first.wpilibj.Notifier
 import frc.team5190.robot.drive.DriveSubsystem
 import frc.team5190.robot.pathreader.MotionProfileTrajectory
 import frc.team5190.robot.sensors.NavX
@@ -21,44 +20,39 @@ class PathfinderCommand(requestId: Int,
                         isReversed: Boolean = false,
                         isMirrored: Boolean = false) : MotionCommand(requestId, isReversed, isMirrored) {
 
-    private var turnControlOutput = 0.0
-
-    private val turnController = PIDController(DriveConstants.TURN_P, DriveConstants.TURN_I, DriveConstants.TURN_D, NavX, PIDOutput { turnControlOutput = it })
-
-    private val leftFollower by lazy {
-        EncoderFollower(convertToPathfinder(leftTrajectory))
-    }
-
-    private val rightFollower by lazy {
-        EncoderFollower(convertToPathfinder(rightTrajectory))
-    }
-
-    /**
-     * @param The motion project trajectory list that is going to be converted to Jaci's Trajectory list
-     * @return The converted trajectory list
-     */
-    private fun convertToPathfinder(motionProfileTrajectory: MotionProfileTrajectory) =
-            Trajectory(motionProfileTrajectory.map {
-                Trajectory.Segment(it.dt, it.x, it.y, it.position, it.velocity, it.acceleration, it.jerk, it.heading)
-            }.toTypedArray())
-
-    init {
-        with(turnController) {
-            setInputRange(-180.0, 180.0)
-            setOutputRange(-1.0, 1.0)
-            setAbsoluteTolerance(5.0)
-            setContinuous(true)
-        }
-    }
-
-    override fun execute() {
+    private val profileThread = Notifier({
         val encoderPosition = with(DriveSubsystem.falconDrive) { leftEncoderPosition + rightEncoderPosition } / 2
 
         val leftOutput = leftFollower.calculate(encoderPosition)
         val rightOutput = rightFollower.calculate(encoderPosition)
-        turnController.setpoint = Pathfinder.boundHalfDegrees((Pathfinder.r2d(leftFollower.heading) + Pathfinder.r2d(rightFollower.heading)) / 2.0)    // Bound to -180..180 degrees
 
-        DriveSubsystem.falconDrive.tankDrive(ControlMode.Velocity, leftOutput + turnControlOutput, rightOutput - turnControlOutput)
+        val gyroHeading = NavX.angle
+        val desiredHeading = Pathfinder.r2d((Pathfinder.r2d(leftFollower.heading) + Pathfinder.r2d(rightFollower.heading)) / 2.0)
+
+        val angleDifference = Pathfinder.boundHalfDegrees(desiredHeading - gyroHeading)
+        val turn = 0.8 * (-1.0 / 80.0) * angleDifference
+
+        DriveSubsystem.falconDrive.tankDrive(ControlMode.PercentOutput, leftOutput + turn, rightOutput - turn)
+    })
+
+    private val leftFollower by lazy { convertToFollower(leftTrajectory) }
+
+    private val rightFollower by lazy { convertToFollower(rightTrajectory) }
+
+    private fun convertToFollower(motionProfileTrajectory: MotionProfileTrajectory) = EncoderFollower(Trajectory(motionProfileTrajectory.map {
+        Trajectory.Segment(it.dt, it.x, it.y, it.position, it.velocity, it.acceleration, it.jerk, it.heading)
+    }.toTypedArray())).apply {
+        configureEncoder(0, DriveConstants.SENSOR_UNITS_PER_ROTATION, DriveConstants.WHEEL_RADIUS / 6.0)
+        configurePIDVA(DriveConstants.P_HIGH, DriveConstants.I_HIGH, DriveConstants.D_HIGH, 1.0 / 9.0, 0.0)
+    }
+
+    override fun initialize() {
+        DriveSubsystem.autoReset()
+        profileThread.startPeriodic(leftTrajectory[0].dt)
+    }
+
+    override fun end() {
+        profileThread.stop()
     }
 
     override fun isFinished() = leftFollower.isFinished || rightFollower.isFinished
