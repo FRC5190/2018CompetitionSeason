45020a682ef3bb0c564092c49fd42abd9aa8e5fa
Vijay Machiraju <vijay.machiraju@gmail.com>
2018-03-16 16:45:24 -0400

    Improved vision processing in thread

--- src/main/kotlin/frc/team5190/robot/vision/Vision.kt
+++ src/main/kotlin/frc/team5190/robot/vision/Vision.kt
@@ -18,37 +18,26 @@ object Vision {
     private const val BAUD_RATE = 115200
 
     init {
-        try {
-            println("[Vision] Creating JeVois SerialPort...")
-            visionPort = SerialPort(BAUD_RATE, SerialPort.Port.kUSB1)
-            println("[Vision] Success!")
-        } catch (e: Exception) {
-            visionPort = null
-            println("[Vision] Failed!")
+        thread(name = "Vision") {
+            while (!stopped) {
+                try {
+                    periodic()
+                    sleep(20)
+                } catch (e: Exception) {
+                    sleep(50)
+                }
+            }
         }
 
-        if (visionPort != null) {
-            thread(name = "Vision") {
-                while (!stopped) {
-                    try {
-                        periodic()
-                        sleep(10)
-                    } catch (e: Exception) {
-                        sleep(50)
-                    }
-                }
+        // we can create the streaming server only after we start reading data from the serial port
+        try {
+            print("[Vision] Starting stream capture...")
+            CameraServer.getInstance().startAutomaticCapture(0).apply {
+                setPixelFormat(VideoMode.PixelFormat.kYUYV)
             }
-
-            // we can create the streaming server only after we start reading data from the serial port
-            try {
-                println("[Vision] Starting stream capture...")
-                CameraServer.getInstance().startAutomaticCapture(0).apply {
-                    setPixelFormat(VideoMode.PixelFormat.kYUYV)
-                }
-                println("[Vision] Success!...")
-            } catch (e: Exception) {
-                println("[Vision] Failed!")
-            }
+            println(" success!")
+        } catch (e: Exception) {
+            println(" failed!")
         }
     }
 
@@ -64,17 +53,30 @@ object Vision {
             val Range: Long)
 
     private fun periodic() {
-        if (visionPort!!.bytesReceived > 0) {
+        if (visionPort == null) {
+            try {
+                print("[Vision] Creating JeVois SerialPort...")
+                visionPort = SerialPort(BAUD_RATE, SerialPort.Port.kUSB1)
+                println(" success!")
+            } catch (e: Exception) {
+                visionPort = null
+                println(" failed!")
+                return
+            }
+        }
+
+        while (visionPort!!.bytesReceived > 0) {
             lastDataReceived = System.currentTimeMillis()
             val string = visionPort!!.readString()
-            println(string)
+//            println(string)
             val obj = gson.fromJson<VisionTemplate>(string)
             isTgtVisible = obj.Track
             tgtAngle = obj.Angle
             tgtRange = obj.Range
         }
-        else if (System.currentTimeMillis() - lastDataReceived > 200) {
-            // no updates for more than 200ms => not tracking
+
+        if (System.currentTimeMillis() - lastDataReceived > 500) {
+            // no updates for more than 500ms => not tracking
             isTgtVisible = 0
             tgtAngle = 0
             tgtRange = 0
