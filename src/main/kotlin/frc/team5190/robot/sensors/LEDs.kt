a651f1b340c845ffbf880ab50476d6c0c84912fa
ThePrimedTNT <13050203+ThePrimedTNT@users.noreply.github.com>
2018-03-30 12:42:14 -0400

    LED cleanup

--- src/main/kotlin/frc/team5190/robot/sensors/LEDs.kt
+++ src/main/kotlin/frc/team5190/robot/sensors/LEDs.kt
@@ -12,44 +12,37 @@ import frc.team5190.robot.climb.ClimbSubsystem
 import frc.team5190.robot.elevator.ElevatorSubsystem
 import frc.team5190.robot.intake.IntakeSubsystem
 import frc.team5190.robot.util.setLEDOutput
+import java.awt.Color
 
 object LEDs : Subsystem() {
 
-    private val leds = CANifier(16)
+    private val canifier = CANifier(16)
+
+    private val COLOR_CLEAR = Color(0, 0, 0)
+    private val COLOR_RED = Color(255, 0, 0)
+    private val COLOR_ORANGE = Color(255, 100, 0)
+    private val COLOR_PURPLE = Color(64, 0, 128)
+    private val COLOR_GREEN = Color(0, 255, 0)
 
     private var blinkedFor = 0L
 
     override fun initDefaultCommand() {}
 
     override fun periodic() {
-        if (!Robot.INSTANCE!!.dataRec || !Robot.INSTANCE!!.isEnabled) {
-            leds.setLEDOutput(0, 0, 0)
-        } else {
-
-            if (!ElevatorSubsystem.closedLpControl) {
-                if (System.currentTimeMillis() % 600 > 300)
-                    leds.setLEDOutput(0, 0, 0)
-                else
-                    leds.setLEDOutput(255, 0, 0)
-            } else if (ClimbSubsystem.climbState) {
-                if (System.currentTimeMillis() % 600 > 300)
-                    leds.setLEDOutput(0, 0, 0)
-                else
-                    leds.setLEDOutput(255, 100, 0)
-            } else if (IntakeSubsystem.isCubeIn) {
+        canifier.setLEDOutput(if (!Robot.INSTANCE!!.dataRec || !Robot.INSTANCE!!.isEnabled)
+            COLOR_CLEAR
+        else when {
+            !ElevatorSubsystem.closedLpControl -> if (System.currentTimeMillis() % 600 > 300) COLOR_CLEAR else COLOR_RED
+            ClimbSubsystem.climbState -> if (System.currentTimeMillis() % 600 > 300) COLOR_CLEAR else COLOR_ORANGE
+            IntakeSubsystem.isCubeIn -> {
                 if (blinkedFor == 0L) blinkedFor = System.currentTimeMillis()
-                if (System.currentTimeMillis() % 400 > 200 && System.currentTimeMillis() - blinkedFor < 2000)
-                    leds.setLEDOutput(0, 0, 0)
-                else {
-                    if (Robot.INSTANCE!!.isAutonomous)
-                        leds.setLEDOutput(64, 0, 128)
-                    else
-                        leds.setLEDOutput(0, 255, 0)
-                }
-            } else {
-                blinkedFor = 0L
-                leds.setLEDOutput(0, 0, 0)
+                if (System.currentTimeMillis() % 400 > 200 && System.currentTimeMillis() - blinkedFor < 2000) COLOR_CLEAR
+                else if (Robot.INSTANCE!!.isAutonomous) COLOR_PURPLE else COLOR_GREEN
             }
-        }
+            else -> {
+                blinkedFor = 0L
+                COLOR_CLEAR
+            }
+        })
     }
 }
