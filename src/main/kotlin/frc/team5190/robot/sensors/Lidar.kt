fc75613f945bf9825e760fe709da3087465b07d9
Ryan Segerstrom <13050203+ThePrimedTNT@users.noreply.github.com>
2018-04-05 12:17:30 -0400

    Dont need anything fancy since this should just be linear

--- src/main/kotlin/frc/team5190/robot/sensors/Lidar.kt
+++ src/main/kotlin/frc/team5190/robot/sensors/Lidar.kt
@@ -8,14 +8,13 @@ package frc.team5190.robot.sensors
 import com.ctre.phoenix.CANifier
 import edu.wpi.first.wpilibj.command.Subsystem
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard
-import org.apache.commons.math3.analysis.interpolation.LinearInterpolator
-import org.apache.commons.math3.analysis.polynomials.PolynomialSplineFunction
+import org.apache.commons.math3.stat.regression.SimpleRegression
 
 object Lidar : Subsystem() {
 
     private val pwmData = DoubleArray(2)
 
-    private val interpolateFunction: PolynomialSplineFunction
+    private val regressionFunction = SimpleRegression()
 
     // All in inches
     private const val minScaleHeight = 48.0
@@ -31,31 +30,23 @@ object Lidar : Subsystem() {
     private var rawDistance = 0.0
 
     init {
-        val interpolator = LinearInterpolator()
-
         // X - Raw Sensor Units
         // Y - Height in Inches
         val data = arrayOf(
                 1200.0 to 48.0,
                 1490.0 to 59.0,
                 1790.0 to 69.0
-
         )
 
-        interpolateFunction = interpolator.interpolate(data.map { it.first }.toDoubleArray(), data.map { it.second }.toDoubleArray())
+        data.forEach { regressionFunction.addData(it.first, it.second) }
     }
 
     override fun periodic() {
         Canifier.getPWMInput(CANifier.PWMChannel.PWMChannel0, pwmData)
 
         rawDistance = pwmData[0]
-        if (interpolateFunction.isValidPoint(rawDistance)) {
-            // If the values are within our calibrated value range, then calculate the height of scale and if bot is under it
-            scaleHeight = interpolateFunction.value(rawDistance)
-            underScale = minScaleHeight - allowedTolerance < scaleHeight && scaleHeight < maxScaleHeight + allowedTolerance
-        } else {
-            underScale = false
-        }
+        scaleHeight = regressionFunction.predict(rawDistance)
+        underScale = minScaleHeight - allowedTolerance < scaleHeight && scaleHeight < maxScaleHeight + allowedTolerance
 
         SmartDashboard.putNumber("Raw Scale Height", rawDistance)
         SmartDashboard.putNumber("Scale Height", scaleHeight)
