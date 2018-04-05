a65921a22067456fd06d7a6606532bc91d75eb2b
Prateek M <prateek.machiraju@outlook.com>
2018-04-05 15:01:02 -0400

    Pre Unbag

--- src/main/kotlin/frc/team5190/robot/drive/FalconDrive.kt
+++ src/main/kotlin/frc/team5190/robot/drive/FalconDrive.kt
@@ -64,7 +64,7 @@ class FalconDrive(val leftMotors: List<WPI_TalonSRX>,
 
         rightMotors.forEach {
             it.setNeutralMode(NeutralMode.Brake)
-            it.setSensorPhase(DriveConstants.IS_RACE_ROBOT)
+            it.setSensorPhase(!DriveConstants.IS_RACE_ROBOT)
             it.configOpenloopRamp(0.0, TIMEOUT)
         }
 
